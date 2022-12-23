74bbe4c1026bfe922b043c6d355cf5389cde7ed4
VovaKyrylenko <110337169+VovaKyrylenko@users.noreply.github.com>
2022-12-23 21:52:12 +0200

    виправлення помилок(23.12.22)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,32 +3,34 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const parent = document.querySelector(".gallery");
 galleryItems.forEach((item) => {
-  const wrapper = document.createElement("div");
-  wrapper.className = "gallery__item";
-  const link = document.createElement("a");
-  link.className = "gallery__link";
-  link.href = item.original;
-  const img = document.createElement("img");
-  img.src = item.preview;
-  img.alt = item.description;
-  img.className = "gallery__image";
-  img.setAttribute("data-source", item.original);
-  link.appendChild(img);
-  wrapper.appendChild(link);
-  parent.appendChild(wrapper);
+  const element = `<div class="gallery__item">
+  <a class="gallery__link" href=${item.original}>
+    <img
+      class="gallery__image"
+      src=${item.preview}
+      data-source=${item.original}
+      alt=${item.description}
+    />
+  </a>
+</div>`;
+  parent.insertAdjacentHTML("beforeend", element);
 });
 
 parent.addEventListener("click", (evt) => {
   evt.preventDefault();
   if (evt.target.classList.contains("gallery__image")) {
     const instance = basicLightbox.create(
-      `<img src="${evt.target.getAttribute("data-source")}">`
+      `<img src="${evt.target.getAttribute("data-source")}">`,
+      {
+        onShow: (instance) => {
+          document.addEventListener("keydown", (event) => {
+            if (event.code == "Escape") {
+              instance.close();
+            }
+          });
+        },
+      }
     );
     instance.show();
-    document.addEventListener("keydown", (event) => {
-      if (event.code == "Escape") {
-        instance.close();
-      }
-    });
   }
 });
